5964840ce565414eab65ba8766b823786aa89917
Oshan12345 <bsagor99@gmail.com>
2021-01-20 23:32:01 +0600

    .cpanel.yml file added

---
+++ index.js
@@ -0,0 +1,23 @@
+var sam = 10;
+
+switch (sam) {
+  case 1000:
+    console.log("hello");
+    break;
+  case 2000:
+    console.log("dsdsds");
+    break;
+  case 10:
+    console.log("ssssssssssssss");
+    break;
+  default:
+    console.log("sdsdsd");
+}
+
+function doubleIt(num) {
+  console.log(num * 2);
+}
+
+var hh = doubleIt(2);
+
+console.log(hh);
